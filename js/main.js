840d4bbe3b4e6209892969ab00008437f9a81ebf
Chris Renshaw <104800006+renners7777@users.noreply.github.com>
2023-02-07 18:58:02 +0000

    main.js changed slightly

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 document.querySelector('button').addEventListener('click', getFetch)
-const header = { 'X-Api-Key': '1lXZquhMgqfLl9lHSNF9FjkZ7yDnIXfWSwYxW0dC' }
+
 function getFetch(){
     const url = "https://api.api-ninjas.com/v1/quotes?category=inspirational"
 
-    fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then((data) => {
-        console.log(data)
+    return fetch(url, {
+        headers: {
+            'x-api-key': '1lXZquhMgqfLl9lHSNF9FjkZ7yDnIXfWSwYxW0dC'
+        },
+    })
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data)
 
-        // document.querySelector('p').innerText = data
-      })
-      .catch((error) => {
-          console.log(`error ${err}`)
-      });
+            document.querySelector('p').innerText = data
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+
 }
       
 
