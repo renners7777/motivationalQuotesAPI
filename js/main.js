d21fd1e0ca5618ee7de1b888b0bbb4cff1bc8908
Chris Renshaw <104800006+renners7777@users.noreply.github.com>
2023-02-07 18:09:28 +0000

    Altered main.js so that data now links to api-ninjas.com

--- js/main.js
+++ js/main.js
@@ -1,24 +1,18 @@
-//Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
-
+const header = { 'X-Api-Key': '1lXZquhMgqfLl9lHSNF9FjkZ7yDnIXfWSwYxW0dC' }
 function getFetch(){
-    const mQuote = ;
-    const url = `=${mQuote}`
+    const url = "https://api.api-ninjas.com/v1/quotes?category=inspirational"
 
-
-  fetch(url)
+    fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then((data) => {
-      //   console.log(data)
-      //   if(data.media_type === "image"){
-      //     document.querySelector('img').src = data.hdurl
-      //   }else if(data.media_type === "video"){
-      //     document.querySelector('iframe').src = data.url
-      //   }
-      //   document.querySelector('h3').innerText = data.explanation
-      // })
+        console.log(data)
+
+        // document.querySelector('p').innerText = data
+      })
       .catch((error) => {
           console.log(`error ${err}`)
       });
-});
+}
+      
 
